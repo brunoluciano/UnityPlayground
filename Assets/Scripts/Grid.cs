a1439c89bd1af48a97a7f78634655f424778d6a8
brunoluciano <bruno.lucian0544@gmail.com>
2020-08-26 00:02:51 -0300

    Adicionado instância dos prefabs no grid

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Lean.Transition;
 
 public class Grid : MonoBehaviour
 {
@@ -9,14 +11,23 @@ public class Grid : MonoBehaviour
     public float boxHeight;
     public GameObject[,] box = new GameObject[9,9];
 
+    private float timer = 0.0f;
+
     void Start()
     {
         for (int i = 0; i < box.GetLength(0); i++) {
-            for (int j = 0; j < box.GetLength(1); j++)
-            {
+            for (int j = 0; j < box.GetLength(1); j++) {
                 // Debug.Log(i + " - " + j);
-                GameObject boxGrid = Instantiate(boxOriginal, new Vector3(i*40-Screen.width/2 + 50, j*40 - Screen.height/2 + 50, 0), Quaternion.identity);
-                boxGrid.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+                GameObject boxGrid = Instantiate(boxOriginal, new Vector3(0, 0, 0), Quaternion.identity);
+                boxGrid.transform.SetParent(GameObject.FindGameObjectWithTag("Grid").transform, false);
+
+                box[i,j] = boxGrid;
+
+                GameObject text = box[i,j].transform.GetChild(0).gameObject;
+
+
+
+                text.GetComponent<Text>().text = (Random.Range(1, 10)).ToString();
             }
         }
     }
@@ -25,5 +36,37 @@ public class Grid : MonoBehaviour
     void Update()
     {
         
+
+        // timer += Time.deltaTime;
+
+        // if(timer >= 1) {
+        //     transform.
+        //     localPositionTransition(new Vector3(10, 10, 10), 0.5f).
+        //     JoinTransition().
+        //     localPositionTransition(new Vector3(1, 1, 1), 0.5f);
+
+        //     Color32 corBox = new Color32(
+        //      ( byte )Random.Range( 0, 255 ),        // R
+        //      ( byte )Random.Range( 0, 255 ),        // G
+        //      ( byte )Random.Range( 0, 255 ),        // B
+        //      ( byte )Random.Range( 125, 255 ) );      // A
+             
+        //     for (int i = 0; i < box.GetLength(0); i++) {
+        //         for (int j = 0; j < box.GetLength(1); j++) {
+        //             // byte r = Random.Range(0, 255);
+        //             // byte g = Random.Range(0, 255);
+        //             // byte b = Random.Range(0, 255);
+                    
+
+        //             box[i,j].GetComponent<Image>().color = corBox;
+
+        //             GameObject text = box[i,j].transform.GetChild(0).gameObject;
+        //             text.GetComponent<Text>().text = (Random.Range(1, 10)).ToString();
+
+
+        //         }
+        //     }
+        //     timer = 0;
+        // }
     }
 }
